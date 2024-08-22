024edc5957bcd08a9d32c5765f02510685da469e
369webforall <369webforall@gmail.com>
2024-08-22 11:19:59 +0545

    fix issue

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,17 @@
 import React from "react";
 
 const HomePage = () => {
-  return <div>HomePage</div>;
+  return (
+    <div>
+      <h1>Home page</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet ratione
+        ducimus voluptates sequi doloribus perspiciatis nam, qui, eligendi,
+        labore tempora placeat error odit ut architecto deserunt! Recusandae
+        adipisci voluptatem distinctio?
+      </p>
+    </div>
+  );
 };
 
 export default HomePage;
